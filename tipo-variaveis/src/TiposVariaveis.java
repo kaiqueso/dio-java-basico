fc18c5d984d796aa71b02f2cf1975fe27fbeecc7
Kaique Oliveira <kaiquee.souzaoliveira@gmail.com>
2024-05-16 15:26:47 -0300

    Tipos e variáveis em Java

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 30;
+        short ano = 2021;
+        int cep = 12345678;
+        long cpf = 12345678901l;
+        float pi = 3.14f;
+        double salario = 1275.50;
+
     }
 }
